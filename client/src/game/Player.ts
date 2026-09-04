5ef1d301e8a15aaf6f380e3ea4ded2efa87f3386
Eric Liu <250847818+ieatpiYT@users.noreply.github.com>
2026-09-04 14:40:11 -0400

    feat(player): vector normalization for diagonal movement

--- client/src/game/Player.ts
+++ client/src/game/Player.ts
@@ -3,6 +3,7 @@ Player.ts
 */
 
 import {Graphics} from "pixi.js";
+import {Input} from './Input'
 
 export class Player extends Graphics
 {
@@ -13,30 +14,30 @@ export class Player extends Graphics
         super();
 
         this.circle(0, 0, 20);
-        this.fill({color: 'red'});
+        this.fill({color: 'brown'});
     }
 
-    move(input: {keys: Record<string, boolean>})
+    move(input: Input): void
     {
-        if (input.keys["KeyW"]) 
+        let dx = 0;
+        let dy = 0;
+
+        // Users can either use WASD or arrows to move
+        if (input.keys["KeyW"] || input.keys["ArrowUp"]) dy -= 1;
+        if (input.keys["KeyS"] || input.keys["ArrowDown"]) dy += 1;
+        if (input.keys["KeyA"] || input.keys["ArrowLeft"]) dx -= 1;
+        if (input.keys["KeyD"] || input.keys["ArrowRight"]) dx += 1;
+
+
+        // Normalize vector to prevent diagonal speed boost
+        if (dx !== 0 && dy !== 0) 
         {
-            this.y -= this.speed;
+            dx *= Math.SQRT1_2;
+            dy *= Math.SQRT1_2;
         }
 
-        if (input.keys["KeyS"]) 
-        {
-            this.y += this.speed;
-        }
-
-        if (input.keys["KeyA"]) 
-        {
-            this.x -= this.speed;
-        }
-
-        if (input.keys["KeyD"]) 
-        {
-            this.x += this.speed;
-        }
+        this.x += dx * this.speed;
+        this.y += dy * this.speed;
     }
 }
 

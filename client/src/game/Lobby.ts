2587a185e4141ad5d8676b83724a182dad8b8723
Eric Liu <250847818+ieatpiYT@users.noreply.github.com>
2026-09-13 22:39:16 -0400

    feat(lobby): add intermission pad

--- client/src/game/Lobby.ts
+++ client/src/game/Lobby.ts
@@ -19,6 +19,12 @@ export class Lobby extends Container
         spawnArea.rect(400, 400, 200, 200);
         spawnArea.fill({color: 'yellow'});
         this.addChild(spawnArea);
+
+        const intermissionPad = new Graphics();
+        intermissionPad.rect(100, 400, 200, 200);
+        intermissionPad.fill({color: 'white'});
+        intermissionPad.stroke({width: 5, color: 'black'});
+        this.addChild(intermissionPad);
     }
 }
 

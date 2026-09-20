99fea2b2a1aaaa433e07cea63a8821a05ad05c7c
Eric Liu <250847818+ieatpiYT@users.noreply.github.com>
2026-09-19 22:32:35 -0400

    feat(lobby): player detection for intermission pad

--- client/src/game/Lobby.ts
+++ client/src/game/Lobby.ts
@@ -4,29 +4,67 @@ Lobby.ts
 
 import {Container, Graphics} from 'pixi.js';
 
+export const INTERMISSION_PAD = 
+{
+    x: 300,
+    y: 0,
+    width: 400,
+    height: 200
+}
+
 export class Lobby extends Container
 {
     constructor()
     {
         super();
 
-        const background = new Graphics();
-        background.rect(0, 0, 2000, 2000);
-        background.fill({color: 'green'});
-        background.stroke({width: 16, color: 0xcc0000, alignment: 1});
+        const background = new Graphics()
+        .rect(0, 0, 2000, 2000)
+        .fill({color: "green"})
+        .stroke({width: 16, color: 0xcc0000, alignment: 1});
+
         this.addChild(background);
 
-        const spawnArea = new Graphics();
-        spawnArea.rect(400, 400, 200, 200);
-        spawnArea.fill({color: 'yellow'});
+        const spawnArea = new Graphics()
+        .rect(400,400,200,200)
+        .fill({color: "yellow"});
+
         this.addChild(spawnArea);
 
-        const intermissionPad = new Graphics();
-        intermissionPad.rect(300, 0, 400, 200);
-        intermissionPad.fill({color: 'white'});
-        intermissionPad.stroke({width: 5, color: 'black'});
+
+        // Intermission pad.
+        const intermissionPad = new Graphics()
+        .rect(
+            INTERMISSION_PAD.x,
+            INTERMISSION_PAD.y,
+            INTERMISSION_PAD.width,
+            INTERMISSION_PAD.height)
+        .fill({color: "white"})
+        .stroke({width: 5, color: "black"});
+
         this.addChild(intermissionPad);
     }
+
+
+    isOnIntermissionPad(x: number, y: number, radius = 22): boolean
+    {
+        return (
+            x + radius >=
+                INTERMISSION_PAD.x &&
+
+            x - radius <=
+                INTERMISSION_PAD.x +
+                INTERMISSION_PAD.width &&
+
+            y + radius >=
+                INTERMISSION_PAD.y &&
+
+            y - radius <=
+                INTERMISSION_PAD.y +
+                INTERMISSION_PAD.height
+        );
+    }
 }
+
 
 
